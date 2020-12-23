7e4930c3ffe5bff50484312073faf492e069a03d
dariodiaz <dario.dad@gmail.com>
2020-12-23 20:47:54 -0300

    Finishing test for contact us form - happy path

--- cypress/integration/contact-us.spec.js
+++ cypress/integration/contact-us.spec.js
@@ -1,6 +1,13 @@
+/// <reference types="Cypress" />
 describe('Test Contact Us form via Webdriveruni', () => {
     it('Should be able to submit a successful submission via contact us form', () => {
-        
+       cy.visit('http://webdriveruniversity.com/Contact-Us/contactus.html')
+       cy.get('[name="first_name"]').type('Dario')
+       cy.get('[name="last_name"]').type('Diaz')
+       cy.get('[name="email"]').type('dario@test.com')
+       cy.get('textarea.feedback-input').type('text test')
+       cy.get('[type="submit"]').click()
+       cy.get('#contact_reply').should('contain', 'Thank You for your Message!')
     });
 
     it('Should not be able to submit a successful submission via contact us form', () => {
