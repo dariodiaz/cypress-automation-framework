121426fd10ade076bc259800aedbb7871305ba34
dariodiaz <dario.dad@gmail.com>
2020-12-23 20:22:51 -0300

    Removing example tests and adding the scheleton for a new test

---
+++ cypress/integration/contact-us.spec.js
@@ -0,0 +1,9 @@
+describe('Test Contact Us form via Webdriveruni', () => {
+    it('Should be able to submit a successful submission via contact us form', () => {
+        
+    });
+
+    it('Should not be able to submit a successful submission via contact us form', () => {
+        
+    });
+});
