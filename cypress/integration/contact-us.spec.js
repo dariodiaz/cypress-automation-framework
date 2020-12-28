a8988dc89d0a5ef53c7abbdbc73c6b72a057b122
dariodiaz <dario.dad@gmail.com>
2020-12-28 18:46:15 -0300

    Challenge about filling contact us form

--- cypress/integration/contact-us.spec.js
+++ cypress/integration/contact-us.spec.js
@@ -10,7 +10,13 @@ describe('Test Contact Us form via Webdriveruni', () => {
        cy.get('#contact_reply').should('contain', 'Thank You for your Message!')
     });
 
-    it('Should not be able to submit a successful submission via contact us form', () => {
-        
+    it.only('Should not be able to submit a successful submission via contact us form', () => {
+        cy.visit('http://webdriveruniversity.com/Contact-Us/contactus.html')
+       cy.get('[name="first_name"]').type('Dario')
+       cy.get('[name="last_name"]').type('Diaz')
+       cy.get('textarea.feedback-input').type('text test')
+       cy.get('[type="submit"]').click()
+       cy.get('body').should('contain', 'Error: all fields are required')
+       cy.get('body').should('contain', 'Error: Invalid email address')
     });
 });
