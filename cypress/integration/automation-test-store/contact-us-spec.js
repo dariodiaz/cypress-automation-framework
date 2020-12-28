93d2f3bb818a5aa9b81b5ba57afa21667061b201
dariodiaz <dario.dad@gmail.com>
2020-12-28 19:08:08 -0300

    Ignoring xhr logs in the runner

--- cypress/integration/automation-test-store/contact-us-spec.js
+++ cypress/integration/automation-test-store/contact-us-spec.js
@@ -1,5 +1,6 @@
 /// <reference types="Cypress" />
 describe('Test Contact Us form via Automation Test Store', () => {
     it('Should be able to submit a successful submission via contact us form', () => {
+        cy.visit('https://automationteststore.com')
     });
 });
