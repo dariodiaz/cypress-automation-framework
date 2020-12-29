ebaba732c85de13a9fff138f9839a10eda8a998c
dariodiaz <dario.dad@gmail.com>
2020-12-29 00:34:36 -0300

    One more step for contact us test - automation test store

--- cypress/integration/automation-test-store/contact-us-spec.js
+++ cypress/integration/automation-test-store/contact-us-spec.js
@@ -2,5 +2,6 @@
 describe('Test Contact Us form via Automation Test Store', () => {
     it('Should be able to submit a successful submission via contact us form', () => {
         cy.visit('https://automationteststore.com')
+        cy.get('selector').type('hola')
     });
 });
