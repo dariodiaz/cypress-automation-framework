50865fbcff242ad0e09375ebd029a30ee81fa800
dariodiaz <dario.dad@gmail.com>
2021-02-18 19:26:02 -0300

    Adding test for the contact us form in testautomationstore

--- cypress/integration/automation-test-store/contact-us-spec.js
+++ cypress/integration/automation-test-store/contact-us-spec.js
@@ -2,6 +2,12 @@
 describe('Test Contact Us form via Automation Test Store', () => {
     it('Should be able to submit a successful submission via contact us form', () => {
         cy.visit('https://automationteststore.com')
-        cy.get('selector').type('hola')
-    });
-});
+        cy.get('.info_links_footer').contains('Contact Us').click()
+        cy.get('.maintext').should('contain', ' Contact Us')
+        cy.get('#ContactUsFrm_first_name').type('Joe')
+        cy.get('#ContactUsFrm_email').type('joe@test.com')
+        cy.get('#ContactUsFrm_enquiry').type('This is a test This is a test This is a test')
+        cy.get('.col-md-6 > .btn').click()
+        cy.get('.mb40').should('contain', 'Your enquiry has been successfully sent to the store owner!')
+    })
+})
