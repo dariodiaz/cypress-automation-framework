797a08c1c115739621875417fe6660c1040700b0
dariodiaz <dario.dad@gmail.com>
2021-04-22 21:48:45 -0300

    Using cy.title to validate the title of a page

--- cypress/integration/webdriver-uni/contact-us.spec.js
+++ cypress/integration/webdriver-uni/contact-us.spec.js
@@ -3,6 +3,7 @@ describe('Test Contact Us form via Webdriveruni', () => {
     it.only('Should be able to submit a successful submission via contact us form', () => {
        cy.visit('http://webdriveruniversity.com/Contact-Us/contactus.html')
        cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
+       cy.title().should('include', 'WebDriver | Contact Us')
        cy.get('[name="first_name"]').type('Dario')
        cy.get('[name="last_name"]').type('Diaz')
        cy.get('[name="email"]').type('dario@test.com')
