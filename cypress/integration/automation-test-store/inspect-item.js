94ecf2a8d0f79b169ef2a7e48659e367aa7af5b3
dariodiaz <dario.dad@gmail.com>
2021-05-26 20:15:41 -0300

    Mores examples of chaining - now using index

--- cypress/integration/automation-test-store/inspect-item.js
+++ cypress/integration/automation-test-store/inspect-item.js
@@ -8,4 +8,8 @@ describe('Inspect Automation Test Store items using chain of commands', () => {
         cy.visit('https://automationteststore.com')
         cy.get('.prdocutname').contains('Skinsheen Bronzer Stick').click()
     })
+    it('Should be able to click on the first item using index', () => {
+        cy.visit('https://automationteststore.com')
+        cy.get('.fixed_wrapper').find('.prdocutname').eq(0).click()
+    })
 })
