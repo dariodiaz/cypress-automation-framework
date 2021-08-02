b6a1780359a5c575a1cab91ef137a232c8c7575f
Dario Diaz <dario.dad@gmail.com>
2021-08-02 19:18:19 -0300

    Use then cypress command to control code execution

--- cypress/integration/automation-test-store/inspect-item.js
+++ cypress/integration/automation-test-store/inspect-item.js
@@ -4,9 +4,11 @@ describe('Inspect Automation Test Store items using chain of commands', () => {
         cy.visit('https://automationteststore.com')
         cy.get('#block_frame_featured_1769 > .thumbnails > :nth-child(1) > .fixed_wrapper > .fixed > .prdocutname').click()
     })
-    it('Should be able to click on the first item using item text', () => {
+    it.only('Should be able to click on the first item using item text', () => {
         cy.visit('https://automationteststore.com')
-        cy.get('.prdocutname').contains('Skinsheen Bronzer Stick').click()
+        cy.get('.prdocutname').contains('Skinsheen Bronzer Stick').click().then(function(itemHeaderText) {
+            console.log("Selected the folowing item: " + itemHeaderText.text())
+        })
     })
     it('Should be able to click on the first item using index', () => {
         cy.visit('https://automationteststore.com')
