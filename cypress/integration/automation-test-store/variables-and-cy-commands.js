ba5f895f10dc6910a62ce62df3b241a321a510f6
Dario Diaz <dario.dad@gmail.com>
2021-08-03 19:26:31 -0300

    Test with variables

--- cypress/integration/automation-test-store/variables-and-cy-commands.js
+++ cypress/integration/automation-test-store/variables-and-cy-commands.js
@@ -1,7 +1,10 @@
 /// <reference types="Cypress" />
 describe('Verifying variables, cypress and jquery commands ', () => {
     it('Navigating to specific product pages', () => {
+        cy.visit('https://automationteststore.com/')
         const makeupLink = cy.get('a[href*="product/category&path="]').contains('Makeup')
         const skincareLink = cy.get('a[href*="product/category&path="]').contains('Skincare')
+        makeupLink.click()
+        skincareLink.click()
     })
 })
