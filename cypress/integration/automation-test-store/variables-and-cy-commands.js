98fffa5c0022d1e8c10323c113b4b5f00913e0fe
Dario Diaz <dario.dad@gmail.com>
2021-08-03 19:32:01 -0300

    Refactor to fix error about element detached from DOM

--- cypress/integration/automation-test-store/variables-and-cy-commands.js
+++ cypress/integration/automation-test-store/variables-and-cy-commands.js
@@ -2,9 +2,7 @@
 describe('Verifying variables, cypress and jquery commands ', () => {
     it('Navigating to specific product pages', () => {
         cy.visit('https://automationteststore.com/')
-        const makeupLink = cy.get('a[href*="product/category&path="]').contains('Makeup')
-        const skincareLink = cy.get('a[href*="product/category&path="]').contains('Skincare')
-        makeupLink.click()
-        skincareLink.click()
+        const listOfLinksText = ['Makeup', 'Skincare']
+        listOfLinksText.forEach(text => cy.get('a[href*="product/category&path="]').contains(text).click())
     })
 })
