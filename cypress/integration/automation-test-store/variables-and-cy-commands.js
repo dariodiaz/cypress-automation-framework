e7ee164c635b5ddd72241679b6342f32dbf3453b
Dario Diaz <dario.dad@gmail.com>
2021-08-03 19:14:26 -0300

    Use variables in a cypress test

---
+++ cypress/integration/automation-test-store/variables-and-cy-commands.js
@@ -0,0 +1,7 @@
+/// <reference types="Cypress" />
+describe('Verifying variables, cypress and jquery commands ', () => {
+    it('Navigating to specific product pages', () => {
+        const makeupLink = cy.get('a[href*="product/category&path="]').contains('Makeup')
+        const skincareLink = cy.get('a[href*="product/category&path="]').contains('Skincare')
+    })
+})
