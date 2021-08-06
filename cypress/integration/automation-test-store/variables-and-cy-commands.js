7a26ade7654c72cd2a7eabdd61b2a9d251661104
Dario Diaz <dario.dad@gmail.com>
2021-08-06 19:13:33 -0300

    Another approach to handle the flow of execution

--- cypress/integration/automation-test-store/variables-and-cy-commands.js
+++ cypress/integration/automation-test-store/variables-and-cy-commands.js
@@ -2,7 +2,12 @@
 describe('Verifying variables, cypress and jquery commands ', () => {
     it('Navigating to specific product pages', () => {
         cy.visit('https://automationteststore.com/')
-        const listOfLinksText = ['Makeup', 'Skincare']
-        listOfLinksText.forEach(text => cy.get('a[href*="product/category&path="]').contains(text).click())
+        //approach 1
+        // const listOfLinksText = ['Makeup', 'Skincare']
+        // listOfLinksText.forEach(text => cy.get('a[href*="product/category&path="]').contains(text).click())
+
+        //approach 2
+        cy.get('a[href*="product/category&path="]').contains('Makeup').click()
+        cy.get('a[href*="product/category&path="]').contains('Skincare').click()
     })
 })
