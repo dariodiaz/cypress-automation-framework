657913005c31d68a7b56ba72af8bcecbe9f3a00a
Dario Diaz <dario.dad@gmail.com>
2021-08-16 13:16:54 -0300

    Ways of validating properties and control order of execution

--- cypress/integration/automation-test-store/variables-and-cy-commands.js
+++ cypress/integration/automation-test-store/variables-and-cy-commands.js
@@ -7,11 +7,21 @@ describe('Verifying variables, cypress and jquery commands ', () => {
         listOfLinksText.forEach(text => cy.get('a[href*="product/category&path="]').contains(text).click())
     })
 
-    it('Validate properties of the contact page', () => {
+    it.only('Validate properties of the contact page', () => {
         cy.visit('https://automationteststore.com/index.php?rt=content/contact')
         //Uses cypress commands and chaining
-
+        cy.contains('#ContactUsFrm', 'Contact Us Form').find('#field_11').should('contain', 'First name')
         //Jquery approach
+        cy.contains('#ContactUsFrm', 'Contact Us Form').then(text => {
+            const firstNameText = text.find('#field_11').text()
+            expect(firstNameText).to.contain('First name')
+
+            //Embedded commands (closure)
+            cy.get('#field_11').then(fnText => {
+                cy.log(fnText.text())
+                cy.log(fnText)
+            })
+        })
 
         //Embedded commands (closure)
         
