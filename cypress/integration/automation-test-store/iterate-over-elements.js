399282b36fc2e04ee7ba5ab7f5c3a2a39227333a
Dario Diaz <dario.dad@gmail.com>
2021-08-16 19:40:20 -0300

    Iterate and perform an action over different elements based on a condition

--- cypress/integration/automation-test-store/iterate-over-elements.js
+++ cypress/integration/automation-test-store/iterate-over-elements.js
@@ -12,5 +12,11 @@ describe('Iterate over elements', () => {
     it('Add specific product to basket', () => {
         cy.visit('https://automationteststore.com/')
         cy.get('a[href*="product/category&path="]').contains('Hair Care').click()
+
+        cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list) => {
+            if($el.text().includes('Curls to straight Shampoo')) {
+                cy.wrap($el).click()
+            }
+        })
     })
 })
