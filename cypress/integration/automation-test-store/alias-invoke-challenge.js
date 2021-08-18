f3981cedfe6fad72ee570b726d0acbd552dea2c0
Dario Diaz <dario.dad@gmail.com>
2021-08-18 19:57:03 -0300

    Get all the normal product's prices and save them into an array

--- cypress/integration/automation-test-store/alias-invoke-challenge.js
+++ cypress/integration/automation-test-store/alias-invoke-challenge.js
@@ -11,8 +11,15 @@ describe('Alias and invoke Challenge', () => {
         cy.visit('https://automationteststore.com/')
         cy.get('.thumbnail').as('productsThumbnails')
 
-        cy.get('@productsThumbnails').find('.oneprice').each(($el, index, $list) => {
-            cy.log($el.text())
+        // cy.get('@productsThumbnails').find('.oneprice').each(($el, index, $list) => {
+        //     cy.log($el.text())
+        // })
+        cy.get('.thumbnail').find('.oneprice').invoke('text').as('itemPrice')
+        cy.get('@itemPrice').then($linkText => {
+            var itemPrice = $linkText.split('$')
+            for(var i = 0; i < itemPrice.length; i++) {
+                cy.log(itemPrice[i])
+            }
         })
     })
 })
