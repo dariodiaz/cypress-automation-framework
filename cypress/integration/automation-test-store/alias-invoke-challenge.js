2ef1ce7fd8c11c5044a4432fcff9144a7c858124
Dario Diaz <dario.dad@gmail.com>
2021-08-17 23:41:41 -0300

    Author solution for alias-invoke challenge

---
+++ cypress/integration/automation-test-store/alias-invoke-challenge.js
@@ -0,0 +1,10 @@
+describe('Alias and invoke Challenge', () => {
+    it('Validate and count all the thumbnails in the homepage', () => {
+        cy.visit('https://automationteststore.com/')
+
+        cy.get('.thumbnail').as('productsThumbnails')
+
+        cy.get('@productsThumbnails').should('have.length', 16)
+        cy.get('@productsThumbnails').find('.productcart').invoke('attr', 'title').should('include', 'Add to Cart')
+    })
+})
