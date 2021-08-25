0c3cb5e143bda05c71692be37c14a496a8d3ff5e
Dario Diaz <dario.dad@gmail.com>
2021-08-24 22:21:32 -0300

    Handle multiple tabs - part 1

--- cypress/integration/webdriver-uni/contact-us.spec.js
+++ cypress/integration/webdriver-uni/contact-us.spec.js
@@ -1,7 +1,9 @@
 /// <reference types="Cypress" />
 describe('Test Contact Us form via Webdriveruni', () => {
     it('Should be able to submit a successful submission via contact us form', () => {
-       cy.visit('http://webdriveruniversity.com/Contact-Us/contactus.html')
+       //cy.visit('http://webdriveruniversity.com/Contact-Us/contactus.html')
+       cy.visit('http://webdriveruniversity.com')
+       cy.get('#contact-us').click({force:true})
        cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
        cy.title().should('include', 'WebDriver | Contact Us')
        cy.url().should('include', 'contactus')
@@ -14,12 +16,14 @@ describe('Test Contact Us form via Webdriveruni', () => {
     });
 
     it('Should not be able to submit a successful submission via contact us form', () => {
-        cy.visit('http://webdriveruniversity.com/Contact-Us/contactus.html')
-       cy.get('[name="first_name"]').type('Dario')
-       cy.get('[name="last_name"]').type('Diaz')
-       cy.get('textarea.feedback-input').type('text test')
-       cy.get('[type="submit"]').click()
-       cy.get('body').contains('Error: all fields are required')
-       cy.get('body').contains('Error: Invalid email address')
+        // cy.visit('http://webdriveruniversity.com/Contact-Us/contactus.html')
+        cy.visit('http://webdriveruniversity.com')
+        cy.get('#contact-us').click({force:true})
+        cy.get('[name="first_name"]').type('Dario')
+        cy.get('[name="last_name"]').type('Diaz')
+        cy.get('textarea.feedback-input').type('text test')
+        cy.get('[type="submit"]').click()
+        cy.get('body').contains('Error: all fields are required')
+        cy.get('body').contains('Error: Invalid email address')
     });
 });
