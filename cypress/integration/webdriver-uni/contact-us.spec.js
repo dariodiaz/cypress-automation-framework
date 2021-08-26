935b04e62ba52f58eea7ed9eb500041767778441
Dario Diaz <dario.dad@gmail.com>
2021-08-25 23:08:52 -0300

    Handle multiple tabs - part 2

--- cypress/integration/webdriver-uni/contact-us.spec.js
+++ cypress/integration/webdriver-uni/contact-us.spec.js
@@ -3,7 +3,7 @@ describe('Test Contact Us form via Webdriveruni', () => {
     it('Should be able to submit a successful submission via contact us form', () => {
        //cy.visit('http://webdriveruniversity.com/Contact-Us/contactus.html')
        cy.visit('http://webdriveruniversity.com')
-       cy.get('#contact-us').click({force:true})
+       cy.get('#contact-us').invoke('removeAttr', 'target').click({force:true})
        cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
        cy.title().should('include', 'WebDriver | Contact Us')
        cy.url().should('include', 'contactus')
@@ -18,7 +18,7 @@ describe('Test Contact Us form via Webdriveruni', () => {
     it('Should not be able to submit a successful submission via contact us form', () => {
         // cy.visit('http://webdriveruniversity.com/Contact-Us/contactus.html')
         cy.visit('http://webdriveruniversity.com')
-        cy.get('#contact-us').click({force:true})
+        cy.get('#contact-us').invoke('removeAttr', 'target').click({force:true})
         cy.get('[name="first_name"]').type('Dario')
         cy.get('[name="last_name"]').type('Diaz')
         cy.get('textarea.feedback-input').type('text test')
