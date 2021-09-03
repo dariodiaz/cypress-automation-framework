815c75368aa7753ec531633ddf166ffe58ea9047
Dario Diaz <dario.dad@gmail.com>
2021-09-02 23:21:18 -0300

    Handle js alerts - part-1

---
+++ cypress/integration/webdriver-uni/alerts.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress" />
+describe('Handle js alerts', () => {
+    it('Confirm js alert contains the correct text', () => {
+       //cy.visit('http://webdriveruniversity.com/Contact-Us/contactus.html')
+       cy.visit('http://webdriveruniversity.com')
+       cy.get('#popup-alerts').invoke('removeAttr', 'target').click({force:true})
+
+       cy.get('#button1').click()
+    })
+});
