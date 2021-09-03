302ff378c189cd231c1187fdc5deb2f233a683d7
Dario Diaz <dario.dad@gmail.com>
2021-09-03 19:36:50 -0300

    Handle a different type of alert - part 2

--- cypress/integration/webdriver-uni/alerts.js
+++ cypress/integration/webdriver-uni/alerts.js
@@ -1,7 +1,6 @@
 /// <reference types="Cypress" />
 describe('Handle js alerts', () => {
     it('Confirm js alert contains the correct text', () => {
-       //cy.visit('http://webdriveruniversity.com/Contact-Us/contactus.html')
        cy.visit('http://webdriveruniversity.com')
        cy.get('#popup-alerts').invoke('removeAttr', 'target').click({force:true})
 
@@ -9,5 +8,17 @@ describe('Handle js alerts', () => {
        cy.on('window:alert', (str) => {
            expect(str).to.equal('I am an alert box!')
        })
+    })
+
+    it('Validate js confirm alert box works correctly when clicking ok', () => {
+        cy.visit('http://webdriveruniversity.com')
+        cy.get('#popup-alerts').invoke('removeAttr', 'target').click({force:true})
+ 
+        cy.get('#button4').click()
+        cy.on('window:alert', (str) => {
+            return true
+        })
+
+        cy.get('#confirm-alert-text').contains('You pressed OK!')
     })
 });
