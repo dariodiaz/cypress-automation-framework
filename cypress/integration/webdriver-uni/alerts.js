15941a895a6a965bb5b418a4b323571e94cfa2a3
Dario Diaz <dario.dad@gmail.com>
2021-09-07 20:38:10 -0300

    Handle alerts using stubs

--- cypress/integration/webdriver-uni/alerts.js
+++ cypress/integration/webdriver-uni/alerts.js
@@ -34,4 +34,20 @@ describe('Handle js alerts', () => {
 
         cy.get('#confirm-alert-text').contains('You pressed Cancel!')
     })
+
+    it('Validate js alert using a stub', () => {
+        cy.visit('http://webdriveruniversity.com')
+        cy.get('#popup-alerts').invoke('removeAttr', 'target').click({force:true})
+ 
+        const stub = cy.stub()
+        cy.on('window:confirm', stub)
+
+        cy.get('#button4').click().then(() => {
+            expect(stub.getCall(0)).to.be.calledWith('Press a button!')
+        }).then(() => {
+            return true
+        }).then(() => {
+            cy.get('#confirm-alert-text').contains('You pressed OK!')
+        })
+    })
 });
