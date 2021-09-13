55c3b32f8f3f9b39e33c6df36fea8f624447d65a
Dario Diaz <dario.dad@gmail.com>
2021-09-13 20:03:24 -0300

    Handle iframe and modal - part 3

--- cypress/integration/webdriver-uni/iframe.js
+++ cypress/integration/webdriver-uni/iframe.js
@@ -9,6 +9,16 @@ describe('Handling IFrame & Modals', () => {
             const body = $iframe.contents().find('body')
             cy.wrap(body).as('iframe')
         })
+
+        cy.get('@iframe').find('#button-find-out-more').click()
+        cy.get('@iframe').find('#myModal').as('modal')
+
+        cy.get('@modal').should(($expectedText) => {
+            const text = $expectedText.text()
+            expect(text).to.include('Welcome to webdriveruniversity.com we sell a wide range of electrical goods such as laptops, game consoles, cameras...')
+        })
+
+        cy.get('@modal').contains('Close').click()
     });
 
 });
