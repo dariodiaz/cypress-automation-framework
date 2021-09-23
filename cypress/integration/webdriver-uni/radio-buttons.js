b8b46c1fb5372616dd364b8d889ab11ee59f4f46
Dario Diaz <dario.dad@gmail.com>
2021-09-23 16:07:40 -0300

    Validate states for radio buttons

--- cypress/integration/webdriver-uni/radio-buttons.js
+++ cypress/integration/webdriver-uni/radio-buttons.js
@@ -7,4 +7,13 @@ describe('Verify radio buttons via Webdriveruni', () => {
         cy.get('#radio-buttons').find('[type="radio"]').first().check()
         cy.get('#radio-buttons').find('[type="radio"]').eq(1).check()
     });
+
+    it('Validate the states of specific radio buttons', () => {
+        cy.visit('http://webdriveruniversity.com')
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})
+
+        cy.get('[value="lettuce"]').should('not.be.checked')
+        cy.get('[value="cabbage"]').should('be.disabled')
+        cy.get('[value="pumpkin"]').should('be.checked')
+     });
 });
