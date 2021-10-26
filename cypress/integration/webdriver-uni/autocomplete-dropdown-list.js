5fd0a1639481f86931e11a9a172ebbb5a0bbd2b0
Dario Diaz <dario.dad@gmail.com>
2021-10-26 20:13:34 -0300

    Test autocomplete - part 2

--- cypress/integration/webdriver-uni/autocomplete-dropdown-list.js
+++ cypress/integration/webdriver-uni/autocomplete-dropdown-list.js
@@ -5,6 +5,18 @@ describe('Verify Autocomplete dropdown lists via webdriveruni', () => {
         cy.get('#autocomplete-textfield').invoke('removeAttr', 'target').click({force:true})
 
         cy.get('#myInput').type('A')
+
+        cy.get('#myInputautocomplete-list > *').each(($el, index, $list) => {
+            const prod = $el.text()
+            const productToSelect = 'Avacado'
+
+            if(prod === productToSelect){
+                $el.click()
+
+                cy.get('#submit-button').click()
+                cy.url().should('include', productToSelect)
+            }
+        })
     });
 
 });
