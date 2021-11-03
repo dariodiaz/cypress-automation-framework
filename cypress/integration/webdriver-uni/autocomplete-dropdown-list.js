8260f50636ac507253b4430a652d213765d250c6
Dario Diaz <dario.dad@gmail.com>
2021-11-03 00:00:02 -0300

    Add small change to autocomplete test

--- cypress/integration/webdriver-uni/autocomplete-dropdown-list.js
+++ cypress/integration/webdriver-uni/autocomplete-dropdown-list.js
@@ -6,6 +6,9 @@ describe('Verify Autocomplete dropdown lists via webdriveruni', () => {
 
         cy.get('#myInput').type('A')
 
+        // try and delete
+        cy.get('#myInput').type('B')
+
         cy.get('#myInputautocomplete-list > *').each(($el, index, $list) => {
             const prod = $el.text()
             const productToSelect = 'Avacado'
