8eb57ff51eec9c0512c6a3ad6dbee2e38c2c3d5d
Dario Diaz <dario.dad@gmail.com>
2022-04-12 22:28:58 -0300

    Data table test - start

---
+++ cypress/integration/webdriver-uni/data-table.js
@@ -0,0 +1,7 @@
+/// <reference types="Cypress" />
+describe("Traversing DOM elements in Cypress", () => {
+  beforeEach(() => {
+    cy.visit("http://webdriveruniversity.com/");
+    cy.get("#data-table").invoke("removeAttr", "target").click({ force: true });
+  });
+});
