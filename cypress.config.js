18267f6d9b933b6513af0ae4151c3c71193250a7
Dario Diaz <dario.dad@gmail.com>
2022-06-14 19:56:52 -0300

    Cypress 10 upgrade

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  videos: false,
+  pageLoadTimeout: 30000,
+  defaultCommandTimeout: 10000,
+  chromeWebSecurity: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+  },
+})
